5da601dcb4527569c781bafe48a6cdfc8d116e17
Maria Tudose <maria.tudose@aalto.fi>
2019-06-19 14:27:42 +0300

    fixed ui not showing

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ let persons = [
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 app.use(cors())
-
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => { 
     return JSON.stringify(req.body) 
